b1c3f02e1b0ca11b33c5807cf55f2b882466a95e
Matías Cabrera <mcabrera@cys.com.ar>
2022-05-19 20:14:17 -0300

    Change text for deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          JENKINS
+          Jenkins
         </p>
         <a
           className="App-link"
