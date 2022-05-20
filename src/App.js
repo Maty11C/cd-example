3afbb164496eeedc7e8f1549637960a805e52c20
Matías Cabrera <mcabrera@cys.com.ar>
2022-05-20 19:09:02 -0300

    Test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Jenkins
+          Deployed from Jenkins
         </p>
         <a
           className="App-link"
